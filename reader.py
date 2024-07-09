4e8acbbbbc22fb44fd5f412bd08f3489eabc2cea
mzeydkurt <138599632+mzeydkurt@users.noreply.github.com>
2024-07-09 15:13:52 +0300

    Add files via upload

---
+++ reader.py
@@ -0,0 +1,14 @@
+from gtts import gTTS
+import playsound3
+
+# Metni belirleyin
+text = "Hi, How Are You Zehra"
+
+# TTS objesini oluşturun
+tts = gTTS(text=text, lang='en')
+
+# Ses dosyasını kaydedin
+tts.save("output1.mp3")
+
+# Ses dosyasını oynatın
+playsound3.playsound("output1.mp3")
